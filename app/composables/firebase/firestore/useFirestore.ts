7eb6aeb7ad1937c7e58bbe0f7c9530ca3af759c8
darienjonathan <darienjonathan@windowslive.com>
2021-12-06 10:18:23 +0700

    refactor firebase related class to hooks

---
+++ app/composables/firebase/firestore/useFirestore.ts
@@ -0,0 +1,98 @@
+import {
+  addDoc,
+  collection,
+  CollectionReference,
+  deleteDoc,
+  doc,
+  DocumentData,
+  DocumentReference,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  QueryDocumentSnapshot,
+  QuerySnapshot,
+  setDoc,
+  updateDoc,
+  getFirestore,
+  Unsubscribe,
+} from 'firebase/firestore'
+import { Ref } from 'nuxt3/dist/app/compat/capi'
+import useFirestoreQueryBuilder from '~/composables/firebase/firestore/useFirestoreQueryBuilder'
+
+const useFirestore = <T>(path: string, parse: (data: DocumentData) => T) => {
+  const firestore = getFirestore(useNuxtApp().$firebase.app)
+  const collectionReference: Ref<CollectionReference> = computed(() => collection(firestore, path))
+  const queryBuilder = useFirestoreQueryBuilder(collectionReference.value)
+
+  const getDocumentReference = (id: string): DocumentReference => doc(collectionReference.value, id)
+
+  const querySnapshotToMap = (qs: QuerySnapshot): Map<string, T> | undefined => {
+    if (qs.empty) return
+    const map = new Map<string, T>()
+    qs.forEach((snapshot: QueryDocumentSnapshot) => {
+      const snapshotData = parse(snapshot.data())
+      map.set(snapshot.id, snapshotData)
+    })
+    return map
+  }
+
+  const getNewId = (): string => {
+    const docRef = doc(collectionReference.value)
+    return docRef.id
+  }
+
+  const loadCollection = async (): Promise<Map<string, T>> => {
+    const querySnapshot = await getDocs(queryBuilder.currentQuery.value)
+    return querySnapshotToMap(querySnapshot)
+  }
+
+  const subscribeCollection = (fn: (map: Map<string, T>) => void): Unsubscribe => {
+    return onSnapshot(queryBuilder.currentQuery.value, (querySnapshot: QuerySnapshot) => {
+      const data = querySnapshot.empty ? null : querySnapshotToMap(querySnapshot)
+      fn(data)
+    })
+  }
+
+  const loadDocument = async (id: string): Promise<T | null> => {
+    const snapshot = await getDoc(getDocumentReference(id))
+    return snapshot.exists ? parse(snapshot.data()) : null
+  }
+
+  const subscribeDocument = (id: string, fn: (data: T | null) => void): Unsubscribe => {
+    return onSnapshot(getDocumentReference(id), querySnapshot => {
+      const data = querySnapshot.exists ? parse(querySnapshot.data()) : null
+      fn(data)
+    })
+  }
+
+  const push = (values: T): Promise<DocumentReference> => {
+    return addDoc(collectionReference.value, parse(values))
+  }
+
+  const set = (id: string, val: T): Promise<void> => {
+    return setDoc(getDocumentReference(id), val)
+  }
+
+  const update = <K extends keyof T>(id: string, val: Record<K, T[K]>): Promise<void> => {
+    return updateDoc(getDocumentReference(id), val)
+  }
+
+  const deleteRecord = (id: string): Promise<void> => {
+    return deleteDoc(getDocumentReference(id))
+  }
+
+  return {
+    queryBuilder,
+    getNewId,
+    loadCollection,
+    subscribeCollection,
+    loadDocument,
+    subscribeDocument,
+    push,
+    set,
+    update,
+    deleteRecord,
+  }
+}
+
+export default useFirestore
---
+++ app/composables/firebase/storage/useStorage.ts
@@ -0,0 +1,100 @@
+import {
+  deleteObject,
+  getDownloadURL as getItemDownloadURL,
+  getStorage,
+  listAll,
+  ref as reference,
+  StorageReference,
+  StringFormat,
+  uploadBytes,
+  UploadMetadata,
+  uploadString,
+} from 'firebase/storage'
+import getMime, { getBase64Mime } from '~/utils/getMime'
+
+interface PutArgs<T extends File | string> {
+  file: T
+  fileName: string
+  customMetadata?: UploadMetadata['customMetadata']
+}
+
+interface PutStringArgs extends PutArgs<string> {
+  stringFormat?: StringFormat
+}
+
+const useStorage = (path: string, allowedTypes?: string[]) => {
+  const storage = getStorage(useNuxtApp().$firebase.app)
+
+  const storageRef = computed(() => reference(storage, path))
+
+  const getFileRef = (fileName: string): StorageReference => reference(storageRef.value, fileName)
+
+  const validate = (type: string): boolean => (allowedTypes ? allowedTypes.includes(type) : true)
+
+  const createMetadata = (
+    customMetadata: UploadMetadata['customMetadata'],
+    type: string | undefined
+  ): UploadMetadata => {
+    return {
+      ...customMetadata,
+      ...(type && { contentType: type }),
+    }
+  }
+
+  const getDownloadURL = (fileName: string): Promise<string> =>
+    getItemDownloadURL(getFileRef(fileName))
+
+  const list = computed(() => listAll(storageRef.value))
+
+  const put = async ({ file, fileName, customMetadata }: PutArgs<File>) => {
+    const type = await getMime(file)
+    const isValidated = validate(type)
+    if (!isValidated) {
+      throw new Error('File validation failed')
+    }
+    try {
+      const metadata = createMetadata(customMetadata, type)
+      const snapshot = await uploadBytes(getFileRef(fileName), file, metadata)
+      return snapshot
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e)
+    }
+  }
+
+  const putString = async ({
+    file,
+    fileName,
+    stringFormat = StringFormat.RAW,
+    customMetadata,
+  }: PutStringArgs) => {
+    let metadata: UploadMetadata = customMetadata
+    if (stringFormat === StringFormat.BASE64) {
+      const type = getBase64Mime(file)
+      const isValidated = validate(type)
+      if (!isValidated) {
+        throw new Error('File validation failed')
+      }
+      metadata = createMetadata(customMetadata, type)
+    }
+    try {
+      const snapshot = await uploadString(getFileRef(fileName), file, stringFormat, metadata)
+      return snapshot
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e)
+    }
+  }
+
+  const deleteItem = (fileName: string) => deleteObject(getFileRef(fileName))
+
+  return {
+    getDownloadURL,
+    list,
+    put,
+    putString,
+    deleteItem,
+  }
+}
+
+export default useStorage
