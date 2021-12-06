8b4cd2c41f8c59245d430dfe9d96f5e7d146e403
darienjonathan <darienjonathan@windowslive.com>
2021-12-06 19:01:37 +0700

    update media related storage hook

--- app/composables/useStorageItems.ts
+++ app/composables/useStorageItems.ts
@@ -4,8 +4,8 @@ const BASE_PATH = '/assets'
 
 const useStorageItems = () => {
   const usePosts = () => useStorage(`${BASE_PATH}/posts`)
-  const useImages = () => useStorage(`${BASE_PATH}/images`, ['image/jpeg', 'image/png'])
-  const useVideos = () => useStorage(`${BASE_PATH}/videos`)
+  const useImages = () => useStorage(`${BASE_PATH}/media/images`, ['image/jpeg', 'image/png'])
+  const useVideos = () => useStorage(`${BASE_PATH}/media/videos`, ['video/mp4', 'video/quicktime'])
   return {
     usePosts,
     useImages,
