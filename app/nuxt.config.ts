a9106cc19f43249bcd6af932cb93dff2dc6772ef
darienjonathan <darienjonathan@windowslive.com>
2021-12-14 09:20:02 +0700

    css: add google icons

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,6 +2,14 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  meta: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons+Outlined',
+      },
+    ],
+  },
   publicRuntimeConfig: {
     firebaseConfig: {
       apiKey: process.env.API_KEY,
