16aed23350b9b2820918d95b62b7496ac43c2f51
darienjonathan <darienjonathan@windowslive.com>
2021-12-31 13:29:21 +0700

    admin layout test page

---
+++ app/pages/admin/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+.admin
+  | HELLO!
+</template>
+<script lang="ts" setup></script>
+<script lang="ts">
+export default {
+  layout: 'admin',
+}
+</script>
+<style lang="scss" scoped></style>
