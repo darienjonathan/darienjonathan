83f3812fa24c5f0e8aa48541c125ebf0d0d8331a
darienjonathan <darienjonathan@windowslive.com>
2022-01-03 08:33:16 +0700

    いらないコード削除

--- app/layouts/admin.vue
+++ app/layouts/admin.vue
@@ -21,36 +21,3 @@ watch(
   }
 )
 </script>
-
-<style lang="scss" scoped>
-@import '@/assets/css/main';
-
-.header {
-  margin-bottom: 24px;
-}
-
-.form {
-  margin-bottom: 24px;
-}
-
-.input {
-  @include pc {
-    @include flex($justify: space-between);
-    &:not(:last-child) {
-      margin-bottom: 20px;
-    }
-  }
-
-  &__label,
-  &__element {
-    @include sp {
-      margin-bottom: 8px;
-    }
-  }
-}
-
-.submit-btn {
-  @include button($bg-color: rgba($navy-blue-light, 0.5), $font-size: $font-sm);
-  width: 100%;
-}
-</style>
