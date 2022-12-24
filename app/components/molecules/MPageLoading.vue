2ec9bcc03c94520e9d4c865cbd29e78d622520dc
Darien Jonathan <darienjonathan@windowslive.com>
2022-12-24 20:39:29 +0700

    create page loading component

---
+++ app/components/molecules/MPageLoading.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+Transition(
+  name="fade"
+  mode="out-in"
+  @enter="defineViewportVariables"
+)
+  template(v-if="isLoading")
+    .loading__wrapper
+      ALoading.loading__ui
+  template(v-else)
+    slot
+</template>
+<script lang="ts" setup>
+import ALoading from '~/components/atoms/ALoading.vue'
+
+defineProps({
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const { defineViewportVariables } = useViewportUnitSizes(false)
+</script>
+<script lang="ts">
+export default {
+  name: 'MPageLoading',
+  components: { ALoading },
+}
+</script>
+<style lang="scss" scoped>
+@import '~/assets/css/main';
+
+.loading {
+  &__wrapper {
+    width: 100vw;
+    height: 100vh;
+  }
+  &__ui {
+    @include absolute-center;
+  }
+}
+</style>
