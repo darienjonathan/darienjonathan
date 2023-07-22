bbc43f4cb7be78bf42d8c2b554c63155e7b75d98
Darien Jonathan <darienjonathan@windowslive.com>
2023-07-22 20:57:29 +0700

    others: remove unused function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,7 @@
-import * as functions from 'firebase-functions'
 import { onCreate as onUserCreate, onDelete as onUserDelete } from 'src/auth/triggers'
 import { onChange as onMediaChange, onDelete as onMediaDelete } from 'src/media/triggers'
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
-})
-
-export { helloWorld, onUserCreate, onUserDelete, onMediaChange, onMediaDelete }
+export { onUserCreate, onUserDelete, onMediaChange, onMediaDelete }
