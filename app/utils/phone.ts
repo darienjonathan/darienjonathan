f190f37135f35b353eff0a4eb9cec10d20417c54
Darien Jonathan <darienjonathan@windowslive.com>
2023-11-17 23:03:31 +0700

    quickfix: add country code

--- app/utils/phone.ts
+++ app/utils/phone.ts
@@ -13,8 +13,20 @@ export const phoneCodeList = [
     number: 81,
   },
   {
+    country: 'Netherlands',
+    number: 31,
+  },
+  {
+    country: 'New Zealand',
+    number: 64,
+  },
+  {
     country: 'Singapore',
     number: 65,
+  },
+  {
+    country: 'Switzerland',
+    number: 41,
   },
   {
     country: 'Taiwan',
